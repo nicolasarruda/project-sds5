34f62cda6f083fac93e9c85f6d63a77bfc4bb6eb
nicolasarruda <nlarruda@hotmail.com>
2021-11-02 21:14:44 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,17 @@
+import DataTable from 'components/DataTable';
+import Footer from 'components/Footer';
+import Navbar from 'components/NavBar';
 
 function App() {
   return (
-    <div> 
-      <h1 className="text-primary">Hello world!</h1>
-    </div>
+    <>
+      <Navbar />
+      <div className="container">
+        <h1 className="text-primary">Hello world!</h1>
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App developed by <a href="https://github.com/nicolasarruda" target="_blank" rel="noreferrer">Nicolas Luchini</a></p>
+                <p className="text-light"><small><strong>Spring React week</strong><br />
+                    Event promoted by the school DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
